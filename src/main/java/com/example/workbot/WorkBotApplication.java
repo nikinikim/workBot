adab997c02e65c2c9f3a179ae2ff66077a27d433
mosunov.nn <colt92@inbox.ru>
2023-05-19 13:35:07 +0500

    Initial commit

---
+++ src/main/java/com/example/workbot/WorkBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.workbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkBotApplication.class, args);
+    }
+
+}
